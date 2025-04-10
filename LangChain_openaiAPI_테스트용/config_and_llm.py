7576324397bc90d9b9d107661c688a7fd6de8b7d
정현우 <lovearamis3@gmail.com>
2025-04-10 19:31:24 +0900

    config_and_llm.py

--- LangChain_openaiAPI_테스트용/config_and_llm.py
+++ LangChain_openaiAPI_테스트용/config_and_llm.py
@@ -1,5 +1,5 @@
 # ✅ 2. OpenAI API 키 설정
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "sk-proj-.."
 
 # ✅ 3. LLM 설정
 llm = ChatOpenAI(
