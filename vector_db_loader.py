0f7350e54406628f2ef5cff4fdb537c91e133a9f
정현우 <lovearamis3@gmail.com>
2025-05-07 17:24:14 +0900

    Delete vector_db_loader.py

--- vector_db_loader.py
+++
@@ -1,6 +0,0 @@
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
-
-def load_vectordb(persist_dir="./chroma_db2"):
-    embedding = OpenAIEmbeddings()
-    return Chroma(persist_directory=persist_dir, embedding_function=embedding)
